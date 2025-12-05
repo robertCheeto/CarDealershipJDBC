69f770989368c5385f4a76fa69c031d719b4ecb0
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-05 11:38:28 -0500

    * copied code from my OOPCarDealership repo for the car information * updated Vehicle variables to match what was contained in my database

--- src/main/java/com/pluralsight/models/Vehicle.java
+++ src/main/java/com/pluralsight/models/Vehicle.java
@@ -1,4 +1,51 @@
 package com.pluralsight.models;
 
 public class Vehicle {
+
+    private int vin, mileage;
+    private String make, model, sold;
+    private double price;
+
+    public Vehicle(int vin, String make, String model, int mileage, double price, String sold) {
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.price = price;
+        this.sold = sold;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSold() {
+        return sold;
+    }
+
+    @Override
+    public String toString() {
+        return "\t VIN: " + vin +
+                "\n\t Mileage: " + mileage +
+                "\n\t Make: " + make +
+                "\n\t Model: " + model +
+                "\n\t Price: " + price +
+                "\n\t Sold: " + sold;
+    }
 }
