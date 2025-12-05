b3fbda7f0b93cac3e42ed97a1351039c60d9cd1e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-05 15:46:34 -0500

    * working on adding code to the database classes

--- src/main/java/com/pluralsight/data/DatabaseDao.java
+++ src/main/java/com/pluralsight/data/DatabaseDao.java
@@ -1,4 +1,19 @@
 package com.pluralsight.data;
 
+import com.pluralsight.models.Vehicle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseDao {
+
+    private final DataManager dataManager;
+
+    public DatabaseDao(DataManager dataManager){
+        this.dataManager = dataManager;
+    }
 }
--- src/main/java/com/pluralsight/ui/DataBaseSeach.java
+++ src/main/java/com/pluralsight/ui/DataBaseSeach.java
@@ -1,4 +1,15 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.data.DataManager;
+import com.pluralsight.data.DatabaseDao;
+
 public class DataBaseSeach {
+
+    private final DatabaseDao databaseDao;
+
+    public DatabaseDao(DataManager dataManager){
+        this.dataManager = dataManager;
+    }
+
+
 }
