687ef9bf041a0252022e0f3ccd4b7ad898616dea
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-05 13:50:22 -0500

    * instantiated VehicleSearch and made a constructor for the HomeScreen to utilize vehicleSearch * added a method that closes the program to the main menu

--- src/main/java/com/pluralsight/ui/HomeScreen.java
+++ src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -1,9 +1,17 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.utilities.ConsoleUtil;
+
 import java.util.Scanner;
 
 public class HomeScreen {
+    private final VehicleSearch vehicleSearch;
 
+    public HomeScreen(VehicleSearch vehicleSearch){
+        this.vehicleSearch = vehicleSearch;
+    }
+
+    // REMOVE SCANNER HERE AND UTILIZE THE ONE IN CONSOLEUTIL
     public void displayHomeScreen(){
         boolean isRunning = true;
         Scanner keyboard = new Scanner(System.in);
@@ -15,10 +23,18 @@ public class HomeScreen {
             keyboard.nextLine();
 
             switch (userChoice){
-                case 1 -> System.out.println("hehe");
+                case 1 -> {
+                    vehicleSearch.display();
+                    break;
+                }
+                case 99 -> {
+                    isRunning = false;
+                    break;
+                }
                 default -> System.out.println("enter a valid input option");
             }
         }
+        exitProgram();
     }
 
     private void displayMenu(){
@@ -30,5 +46,9 @@ public class HomeScreen {
         System.out.println("99) Close Program");
         System.out.println("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
         System.out.print("Enter option here: ");
+    }
+
+    private void exitProgram(){
+        ConsoleUtil.exitProgram();
     }
 }
