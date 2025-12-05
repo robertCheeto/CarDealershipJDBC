41cd40695e5c5fd1b2c05e6108276f6c827c7786
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-05 11:28:08 -0500

    * created the homescreen menu and built out structure for what needs to be added

--- src/main/java/com/pluralsight/ui/HomeScreen.java
+++ src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -1,4 +1,34 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class HomeScreen {
+
+    public void displayHomeScreen(){
+        boolean isRunning = true;
+        Scanner keyboard = new Scanner(System.in);
+
+        while (isRunning){
+            displayMenu();
+
+            int userChoice = keyboard.nextInt();
+            keyboard.nextLine();
+
+            switch (userChoice){
+                case 1 -> System.out.println("hehe");
+                default -> System.out.println("enter a valid input option");
+            }
+        }
+    }
+
+    private void displayMenu(){
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        System.out.println("Select Option Based on Menu Number\n\n");
+        System.out.println("1) Search For Vehicles");
+        System.out.println("2) Add/Remove Vehicles");
+        System.out.println("3) Sales/Leasing Vehicles");
+        System.out.println("99) Close Program");
+        System.out.println("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        System.out.print("Enter option here: ");
+    }
 }
