e4048a86499963efd63be4617766dc7df09cffbe
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-05 15:01:34 -0500

    * updated Vehicle object class to contain new variables from updated database

--- src/main/java/com/pluralsight/models/Vehicle.java
+++ src/main/java/com/pluralsight/models/Vehicle.java
@@ -2,25 +2,37 @@ package com.pluralsight.models;
 
 public class Vehicle {
 
-    private int vin, mileage;
-    private String make, model, sold;
+    private int year, odometer, sold;
+    private String vin, make, model, vehicleType, color;
     private double price;
 
-    public Vehicle(int vin, String make, String model, int mileage, double price, String sold) {
+    public Vehicle(String vin, int year, String make, String model, String vehicleType, String color,
+                   int odometer, double price, int sold) {
         this.vin = vin;
+        this.year = year;
         this.make = make;
         this.model = model;
-        this.mileage = mileage;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
         this.price = price;
         this.sold = sold;
     }
 
-    public int getVin() {
-        return vin;
+    public int getYear() {
+        return year;
     }
 
-    public int getMileage() {
-        return mileage;
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public String getVin() {
+        return vin;
     }
 
     public String getMake() {
@@ -31,21 +43,24 @@ public class Vehicle {
         return model;
     }
 
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     public double getPrice() {
         return price;
     }
 
-    public String getSold() {
-        return sold;
-    }
-
     @Override
     public String toString() {
-        return "\t VIN: " + vin +
-                "\n\t Mileage: " + mileage +
-                "\n\t Make: " + make +
-                "\n\t Model: " + model +
-                "\n\t Price: " + price +
-                "\n\t Sold: " + sold;
+        StringBuilder sb = new StringBuilder();
+        sb.append("VIN: ").append(vin).append("\tYear: ").append(year).append("\tMake: ").append(make);
+        sb.append("\tModel: ").append(model).append("\tVehicle Type: ").append(vehicleType).append("\tColor: ");
+        sb.append(color).append("\tOdometer: ").append(odometer).append("\tPrice: $").append(price).append("Sold: ").append(sold);
+        return sb.toString();
     }
 }
